2278f24079e2b7b692e73ddf5d9bf16d3fc00804
onkardeolankar <96459819+onkardeolankar@users.noreply.github.com>
2022-06-11 00:36:25 +0530

    Add files via upload

---
+++ day-30_queries.sql
@@ -0,0 +1,7 @@
+create table zenclass(Student_Collection varchar(20),Batch_Collection int,Task_Collection varchar(20),Mentor_Collection varchar(10));
+select * from zenclass;
+
+insert into zenclass(Student_Collection,Batch_Collection,Task_Collection,Mentor_Collection) values ('Onkar',32,'Design a form','RaghavK');
+insert into zenclass(Student_Collection,Batch_Collection,Task_Collection,Mentor_Collection) values ('Darshan',32,'Create a HTML Page','SumanG');
+insert into zenclass(Student_Collection,Batch_Collection,Task_Collection,Mentor_Collection) values ('Nishant',32,'Build using API','LavishG');
+insert into zenclass(Student_Collection,Batch_Collection,Task_Collection,Mentor_Collection) values ('Krittika',32,'Design Toggle Button','GuviM');
